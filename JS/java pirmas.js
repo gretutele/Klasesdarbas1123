00bef0893dbb281128ac902483ba326515f78bb0
Greta Strigune <gkaraskaite@gmail.com>
2021-11-30 21:51:44 +0200

    pirmas su java, java neveikia kazkodel

--- JS/java pirmas.js
+++ JS/java pirmas.js
@@ -1,10 +1,37 @@
+<!--
 let a = 1;
 let tekstas = 'Kintamojo a reiksme: ';
-
 console.log(tekstas + a);
-
 a = 2;
-
 console.log(tekstas + a);
 
+let b = 'Greta'
+let e = ' '
+let c = 'Mano vardas'
+console.log(b + e + c);
 
+let metai = 1989;
+let menuo = 10;
+let diena = 25;
+let a1 = metai % 10;
+let a2 = menuo % 10;
+let a3 = diena % 10;
+let ats = a1 + a2 + a3;
+console.log(ats);
+-->
+
+let n1 = 3;
+let n2 = 4;
+
+let s1 = (n1 - 2) * 180;
+let s2 = (n2 - 2) * 180;
+
+document.getElementById( elementid: 'ats')
+.innerHTML = 'Pirmo daugiakampio [' + n1 + '] kampu suma yra ' + s1 + ' ';
+document.getElementById( elementid: 'ats')
+.innerHTML += 'Antrojo daugiakampio [' + n2 + '] kampu suma yra ' + s2 + ' ';
+document.getElementById( elementid: 'ats')
+.innerHTML += 'Abieju kampu suma yra ' + (s1 + s2);
+
+let elementasAts = document.getElementById(elementid: 'ats'
+);
