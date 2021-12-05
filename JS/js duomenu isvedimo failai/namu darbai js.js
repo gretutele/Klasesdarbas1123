8880359806b77d6e3b63536a3f8e55d4a2feb158
Greta Strigune <gkaraskaite@gmail.com>
2021-12-05 14:49:24 +0200

    namu darbai, ties numeral strigimas

--- JS/js duomenu isvedimo failai/namu darbai js.js
+++ JS/js duomenu isvedimo failai/namu darbai js.js
@@ -1,4 +1,4 @@
-<!--  Apskritimo ilgis ir plotas-->
+// Apskritimo ilgis ir plotas //
 
 let r = 15;
 let ats1;
@@ -10,7 +10,7 @@ ats2 = r * 3.14 * 2;
 console.log(ats1);
 console.log(ats2);
 
-<!-- Duomenys-->
+// Duomenys //
 
 let vardas = "Sima";
 let pavarde = "Rimaite";
@@ -20,7 +20,11 @@ let pazymiai = "jos pazymiai [8, 3, 9]";
 
 console.log(vardas, pavarde, kursas, pavadinimas, pazymiai);
 
-<!-- typeof-->
+let sentence = 'vardas yra Sima, pavarde yra Rimaite, kursas 4, ' +
+    'mokosi "KTU", jos pazymiai [8, 3, 9]';
+document.getElementById("demo").innerHTML = sentence + "<br>";
+
+// typeof //
 
 console.log(typeof 'Sima');
 console.log(typeof 'Rimaite');
@@ -28,7 +32,7 @@ console.log(typeof 4);
 console.log(typeof 'mokosi KTU');
 console.log(typeof []);
 
-<!--  Objektas studente -->
+//  Objektas studente //
 
 const studente = {
     vardas1: "Sima",
@@ -37,12 +41,12 @@ const studente = {
     pavadinimas1: "KTU",
     pazymiai1: [8, 3, 9]
 };
-<!--   masyvas/konsole.log 5/6  -->
+// masyvas/konsole.log 5/6 //
 
 let masyvas = ["Sima", "Rimaite", 4, "KTU", [8, 3, 9]];
 console.log(masyvas);
 
-<!--  dot ir bracket -->
+//  dot ir bracket //
 
 let dot = "Dot notation: " +
     studente.vardas1 + " " +
@@ -60,19 +64,30 @@ let bracket = "Bracket notation: " +
     studente['pazymiai1'] + "]";
 console.log(bracket);
 
-<!--  pazymiai -->
+// pazymiai //
 
 for (let i = 0; i < masyvas.length; i++) {
-    document.getElementById("pazymiai").innerHTML = masyvas[4];
+   document.getElementById("pazymiai").innerHTML = masyvas[4];
 }
 
-<!--    pakelti per viena  -->
+// pakelti per viena  //
 
-let vienu_daugiau;
+let x = 8; y = 3; z = 9;
+x++ , y++, z++;
+console.log(x, y, z);
 
-if (a === 8) {
-    prideti = (a++);
-}
-    console.log(a);
+// suma
+
+let sum = 8 + 3 + 9;
+document.getElementById("sum").innerHTML = sum;
+
+// daugyba
+
+let daug = 8 * 3;
+document.getElementById("daug").innerHTML = daug;
+
+// ar kursas numeral tipo
+
+
 
 
